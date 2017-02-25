e64e9ffc059245e4419dd8d150fbee6ab1ad6f81
Marek Sikorski <marek.sikorski1@gmail.com>
2017-02-25 23:00:28 +0100

    add new file

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
 var a =prompt('Podaj liczbe');
 var b =prompt('Podaj liczbe');
 var value = (a*a) + (2 * a * b) - (b*b);
-if (value<0){
+if (value<0) {
 	console.log('Liczba jest ujemna');
 }
 else if (value>0) {
 	console.log('Liczba jest dodatnia');
 }
+(value==0)?
+	console.log('Wynik jwst równy zero!'):console.log('Wynik nie jest równy zero!');
+
 	console.log('Liczba wynosi:'+value);
 	alert('Liczba wynosi:'+value);
